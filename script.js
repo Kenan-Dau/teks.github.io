43eec456231c44a67b41365a81b51878243816f4
Kenn-14 <necromtouch@gmail.com>
2025-10-08 09:06:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,146 +1,194 @@
 $(document).ready(function () {
-    /*
-     * Main variables
-     */
-    var content = [{
-        title: "I Have Crush On You!!",
-        desc: ""
-    }, {
-        title: "I Have Crush On You!!",
-        desc: "Mungkin itu sebuah kalimat yg cukup simple, kalimat yang menyimpan banyak harapan dan juga keraguan dibaliknya. Ada harapan yg ingin rasa suka itu tidak jatuh sendirian, juga ada yg harus disiapkan agar bisa menerima resiko menyukai seseorang."
-    }, {
-       title: "",
-       desc: "Menyatakan perasaan itu hal yg cukup serius, banyak yg akan dikorbankan termasuk hasil akhirnya, juga melepas hal yg sudah Iama tersimpan itu cukup melegakan dan bukan hal yg mudah. Sebenarnya aku nggak hebat masalah ungkapin perasaan tapi disini aku cuman mau ungkapin perasaan aku sama kamu, masalah diterima atau nggak itu hak kamu, aku juga gak bakal berharap lebih kok"
-    }, {
-        title: "",
-        desc: "Kalau kamu risih bilang yahh gausa dipendam atau takut nyakitin perasaan aku, semisal kamu udah punya pacar maaf yaa karna udah confess ke kamu...mungkin segitu saja dari aku, hehe makasih yaa sudah mau dibaca."
-    }, {
-        title: "I Have Crush On You!!",
-        desc: "Aku Tunggu Balasan Mu:)"
- }];
-    var currentPage = 0;
-    //generate content
-    for (var i = 0; i < content.length; i++) {
-        //split content letters to array
-        for (var obj in content[i]) {
-            //if string
-            if (typeof content[i][obj] === "string") {
-                content[i][obj] = content[i][obj].split("");
-                continue;
-            }
-            //if array (grouped text)
-            else if (typeof content[i][obj] === "object") {
-                var toPush = [];
-                for (var j = 0; j < content[i][obj].length; j++) {
-                    for (var k = 0; k < content[i][obj][j].length; k++) {
-                        toPush.push(content[i][obj][j][k]);
-                    }
-                }
-                content[i][obj] = toPush;
-            }
-        }
-        //set text to 
-        $("#segments").append("<div class=\"letters-wrap mutable\"><div class=\"soup-title\"></div><div class=\"soup-desc\"></div></div>");
-        setText();
-        //clone to data
-        $("#segments").append("<div class=\"letters-wrap position-data\"><div class=\"soup-title\"></div><div class=\"soup-desc\"></div></div>");
-        setText();
+  /*
+   * Main variables
+   */
+  // Menggunakan 'const' karena array content tidak di-assign ulang
+  const content = [
+    {
+      title: "I Have Crush On You!!",
+      desc: "",
+    },
+    {
+      title: "I Have Crush On You!!",
+      desc: "Mungkin itu sebuah kalimat yg cukup simple, kalimat yang menyimpan banyak harapan dan juga keraguan dibaliknya. Ada harapan yg ingin rasa suka itu tidak jatuh sendirian, juga ada yg harus disiapkan agar bisa menerima resiko menyukai seseorang.",
+    },
+    {
+      title: "",
+      desc: "Menyatakan perasaan itu hal yg cukup serius, banyak yg akan dikorbankan termasuk hasil akhirnya, juga melepas hal yg sudah Iama tersimpan itu cukup melegakan dan bukan hal yg mudah. Sebenarnya aku nggak hebat masalah ungkapin perasaan tapi disini aku cuman mau ungkapin perasaan aku sama kamu, masalah diterima atau nggak itu hak kamu, aku juga gak bakal berharap lebih kok",
+    },
+    {
+      title: "",
+      desc: "Kalau kamu risih bilang yahh gausa dipendam atau takut nyakitin perasaan aku, semisal kamu udah punya pacar maaf yaa karna udah confess ke kamu...mungkin segitu saja dari aku, hehe makasih yaa sudah mau dibaca.",
+    },
+    {
+      title: "I Have Crush On You!!",
+      desc: "Aku Tunggu Balasan Mu:)",
+    },
+  ];
+
+  // Menggunakan 'let' karena currentPage akan diubah
+  let currentPage = 0;
+
+  // Sekarang menerima 'i' (indeks konten) sebagai parameter
+  function setText(contentIndex) {
+    // Menggunakan 'let' untuk loop counter
+    let j;
+
+    for (j = 0; j < content[contentIndex].title.length; j++) {
+      $(".soup-title")
+        .last()
+        .append(
+          '<span class="letter">' + content[contentIndex].title[j] + "</span>"
+        );
     }
-    //initial arrangement
+    for (j = 0; j < content[contentIndex].desc.length; j++) {
+      $(".soup-desc")
+        .last()
+        .append(
+          '<span class="letter">' + content[contentIndex].desc[j] + "</span>"
+        );
+    }
+  }
+
+  // Menggunakan 'let' untuk loop counter
+  for (let i = 0; i < content.length; i++) {
+    for (let obj in content[i]) {
+      if (typeof content[i][obj] === "string") {
+        content[i][obj] = content[i][obj].split("");
+        continue;
+      } else if (typeof content[i][obj] === "object") {
+        let toPush = [];
+        for (let j = 0; j < content[i][obj].length; j++) {
+          for (let k = 0; k < content[i][obj][j].length; k++) {
+            toPush.push(content[i][obj][j][k]);
+          }
+        }
+        content[i][obj] = toPush;
+      }
+    }
+
+    $("#segments").append(
+      '<div class="letters-wrap mutable"><div class="soup-title"></div><div class="soup-desc"></div></div>'
+    );
+    setText(i);
+
+    $("#segments").append(
+      '<div class="letters-wrap position-data"><div class="soup-title"></div><div class="soup-desc"></div></div>'
+    );
+    setText(i);
+  }
+
+  arrangeCurrentPage();
+  scrambleOthers();
+
+  /*
+   * Event handlers
+   */
+  $(window).resize(function () {
     arrangeCurrentPage();
     scrambleOthers();
-    /*
-     * Event handlers
-     */
-    $(window).resize(function () {
-        arrangeCurrentPage();
-        scrambleOthers();
-    });
-    $("#soup-prev").hide();
-    $("#soup-prev").click(function () {
-        $("#soup-next").show();
-        currentPage--;
-        if (currentPage === 0) {
-            $("#soup-prev").hide();
-        }
-        arrangeCurrentPage();
-        scrambleOthers();
-    });
-    $("#soup-next").click(function () {
-        $("#soup-prev").show();
-        currentPage++;
-        if (currentPage === content.length - 1) {
-            $("#soup-next").hide();
-        }
-        arrangeCurrentPage();
-        scrambleOthers();
-    });
-    /*
-     * Functions
-     */
-    function arrangeCurrentPage() {
-        for (var i = 0; i < content[currentPage].title.length; i++) {
-            $(".mutable:eq(" + currentPage + ") > .soup-title > .letter").eq(i).css({
-                left: $(".position-data:eq(" + currentPage + ") > .soup-title > .letter").eq(i).offset().left + "px",
-                top: $(".position-data:eq(" + currentPage + ") > .soup-title > .letter").eq(i).offset().top + "px",
-                color: "#111",
-                zIndex: 9001
+  });
+
+  // Sembunyikan tombol 'Prev' di awal
+  $("#soup-prev").hide();
+
+  $("#soup-prev").click(function () {
+    $("#soup-next").show();
+    currentPage--;
+    if (currentPage === 0) {
+      $("#soup-prev").hide();
+    }
+    arrangeCurrentPage();
+    scrambleOthers();
+  });
+
+  $("#soup-next").click(function () {
+    $("#soup-prev").show();
+    currentPage++;
+    if (currentPage === content.length - 1) {
+      $("#soup-next").hide();
+    }
+    arrangeCurrentPage();
+    scrambleOthers();
+  });
+
+  /*
+   * Functions
+   */
+  function arrangeCurrentPage() {
+    // Gunakan 'let' untuk loop counter
+    for (let i = 0; i < content[currentPage].title.length; i++) {
+      $(".mutable:eq(" + currentPage + ") > .soup-title > .letter")
+        .eq(i)
+        .css({
+          left:
+            $(".position-data:eq(" + currentPage + ") > .soup-title > .letter")
+              .eq(i)
+              .offset().left + "px",
+          top:
+            $(".position-data:eq(" + currentPage + ") > .soup-title > .letter")
+              .eq(i)
+              .offset().top + "px",
+          color: "#111",
+          zIndex: 9001,
+        });
+    }
+    // Gunakan 'let' untuk loop counter
+    for (let i = 0; i < content[currentPage].desc.length; i++) {
+      $(".mutable:eq(" + currentPage + ") > .soup-desc > .letter")
+        .eq(i)
+        .css({
+          left:
+            $(".position-data:eq(" + currentPage + ") > .soup-desc > .letter")
+              .eq(i)
+              .offset().left + "px",
+          top:
+            $(".position-data:eq(" + currentPage + ") > .soup-desc > .letter")
+              .eq(i)
+              .offset().top + "px",
+          color: "#111",
+          zIndex: 9001,
+        });
+    }
+  }
+
+  function scrambleOthers() {
+    // Gunakan 'let' untuk loop counter
+    for (let i = 0; i < content.length; i++) {
+      if (currentPage === i) continue;
+
+      const parts = [
+        ["title", ".soup-title"],
+        ["desc", ".soup-desc"],
+      ];
+
+      for (let j = 0; j < parts.length; j++) {
+        for (let k = 0; k < content[i][parts[j][0]].length; k++) {
+          const randLeft = Math.floor(Math.random() * $(window).width());
+          const randTop = Math.floor(Math.random() * $(window).height());
+
+          const offset = $(".position-data").eq(currentPage).offset();
+          const bounds = {
+            left: offset.left,
+            top: offset.top,
+            right: $(window).width() - offset.left,
+            bottom: $(window).height() - offset.top,
+          };
+          const middleX =
+            bounds.left + $(".position-data").eq(currentPage).width() / 2;
+          const middleY =
+            bounds.top + $(".position-data").eq(currentPage).height() / 2;
+
+          $(".mutable:eq(" + i + ") > " + parts[j][1] + " > .letter")
+            .eq(k)
+            .css({
+              left: randLeft,
+              top: randTop,
+              color: "#DDD",
+              zIndex: "initial",
             });
         }
-        for (var i = 0; i < content[currentPage].desc.length; i++) {
-            $(".mutable:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).css({
-                left: $(".position-data:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).offset().left + "px",
-                top: $(".position-data:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).offset().top + "px",
-                color: "#111",
-                zIndex: 9001
-            });
-        }
+      }
     }
-
-    function setText() {
-        var j;
-        for (j = 0; j < content[i].title.length; j++) {
-            $(".soup-title").last().append("<span class=\"letter\">" + content[i].title[j] + "</span>");
-        }
-        for (j = 0; j < content[i].desc.length; j++) {
-            $(".soup-desc").last().append("<span class=\"letter\">" + content[i].desc[j] + "</span>");
-        }
-    }
-
-    function scrambleOthers() {
-        for (var i = 0; i < content.length; i++) {
-            //don't scramble currentPage
-            if (currentPage === i)
-                continue;
-            var parts = [
-                ["title", ".soup-title"],
-                ["desc", ".soup-desc"]
-            ];
-            //apply to .title h1s and .desc ps
-            for (var j = 0; j < parts.length; j++) {
-                for (var k = 0; k < content[i][parts[j][0]].length; k++) {
-                    //define random position on screen
-                    var randLeft = Math.floor(Math.random() * $(window).width());
-                    var randTop = Math.floor(Math.random() * $(window).height());
-                    //defining boundaries
-                    var offset = $(".position-data").eq(currentPage).offset();
-                    var bounds = {
-                        left: offset.left,
-                        top: offset.top,
-                        right: $(window).width() - offset.left,
-                        bottom: $(window).height() - offset.top
-                    };
-                    var middleX = bounds.left + $(".position-data").eq(currentPage).width() / 2;
-                    var middleY = bounds.top + $(".position-data").eq(currentPage).height() / 2;
-                    //finally, apply all the scrambles
-                    $(".mutable:eq(" + i + ") > " + parts[j][1] + " > .letter").eq(k).css({
-                        left: randLeft,
-                        top: randTop,
-                        color: "#DDD",
-                        zIndex: "initial"
-                    });
-                }
-            }
-        }
-    }
+  }
 });
